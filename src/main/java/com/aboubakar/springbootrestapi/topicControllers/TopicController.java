ef7c41344dc1be8f5566d77be0aa596d05b25654
AboubakarAhamada <aboumarshal@hotmail.fr>
2022-03-26 11:55:09 +0100

    add, update and delete contol methods

--- src/main/java/com/aboubakar/springbootrestapi/topicControllers/TopicController.java
+++ src/main/java/com/aboubakar/springbootrestapi/topicControllers/TopicController.java
@@ -2,7 +2,9 @@ package com.aboubakar.springbootrestapi.topicControllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,5 +26,20 @@ public class TopicController {
     @RequestMapping("/topics")
     public List<Topic> getAllTopics(){
         return topicService.getAllTopics();
+    }
+
+    @RequestMapping(method=RequestMethod.POST, value ="/topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method= RequestMethod.PUT, value ="/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+        topicService.updateTopic(topic, id);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id){
+        topicService.deleteTopic(id);
     }
 }
