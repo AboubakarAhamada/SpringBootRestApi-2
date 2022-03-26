b86392abf2236ac00fadc0830ab3f915a324eedd
AboubakarAhamada <aboumarshal@hotmail.fr>
2022-03-26 11:53:33 +0100

    Methods to add, update and delete topic

--- src/main/java/com/aboubakar/springbootrestapi/topicService/TopicService.java
+++ src/main/java/com/aboubakar/springbootrestapi/topicService/TopicService.java
@@ -2,6 +2,7 @@ package com.aboubakar.springbootrestapi.topicService;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,12 +11,12 @@ import com.aboubakar.springbootrestapi.topic.Topic;
 @Service
 public class TopicService {
 
-    private List <Topic> topics = Arrays.asList(
+    private List <Topic> topics = new ArrayList<>(Arrays.asList(
         new Topic("python", "Python", "Python and Jango"),
         new Topic("php", "PHP", "PHP & Wordpress"),
         new Topic("java", "Jave", "Java SE and JavaFX"),
         new Topic("jee", "Java", "Java EE and Spring Boot")
-    );
+    ));
     
     public Topic getOneTopic(String id){
         return topics.stream().filter(p -> p.getId().equals(id)).findFirst().orElse(null);
@@ -24,5 +25,25 @@ public class TopicService {
     public List<Topic> getAllTopics(){
         return topics;
     }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(Topic topic, String id){
+        for(int i =0; i < topics.size(); i++){
+            if(topics.get(i).getId().equals(id)){
+                topics.set(i, topic);
+                return;
+            }
+        }
+       
+
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
+    }
+
     
 }
